5a51590f6522c98802ad44eceefe236da6019278
artem <artemqq5@gmail.com>
2023-09-15 01:05:03 +0300

    initial commit

---
+++ app/src/main/java/com/educational/insertsort/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.educational.insertsort.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.educational.insertsort.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
